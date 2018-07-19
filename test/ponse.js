9b3d6ca9cea1236fe8742e0f962037c4bcb291e9
coderaiser <coderaiser@cloudcmd.io>
2018-07-19 15:24:08 +0300

    fix(ponse) path traversal

---
+++ test/ponse.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const test = require('tape');
+const {get} = require('./connect');
+
+test('ponse: path traversal: statusCode', async (t) => {
+    const {statusCode} = await get('../../../../../../etc/passwd', __dirname);
+    
+    t.equal(statusCode, 404, 'should equal');
+    t.end();
+});
+
+test('ponse: path traversal: message', async (t) => {
+    const {body} = await get('../../../../../../etc/passwd', __dirname);
+    const expect = 'Path /etc/passwd beyond root /home/coderaiser/ponse/test!';
+    
+    t.equal(body, expect, 'should equal');
+    t.end();
+});
+
